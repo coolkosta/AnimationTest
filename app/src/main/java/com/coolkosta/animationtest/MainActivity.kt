995e6666828b77d524489a15db674031cd47001f
coolkosta <kostia.borzakov@gmail.com>
2022-10-05 21:33:15 +0300

    app

---
+++ app/src/main/java/com/coolkosta/animationtest/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.coolkosta.animationtest
+
+import android.animation.AnimatorSet
+import android.animation.ArgbEvaluator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
+import android.graphics.Color
+import android.os.Bundle
+import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnRepeat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.imageView).setOnClickListener {
+            startAnimate()
+        }
+    }
+
+    private fun startAnimate() {
+        val rotationXHolder = PropertyValuesHolder.ofFloat("rotationX", 0f, -300f)
+        val colorHolder = PropertyValuesHolder.ofObject(
+            "color", ArgbEvaluator(),
+            Color.GREEN,
+            Color.BLUE,
+            Color.RED
+        )
+        val xAnimatorAndColor =
+            ValueAnimator.ofPropertyValuesHolder(rotationXHolder, colorHolder).apply {
+                interpolator = BounceInterpolator()
+                duration = 2000
+                addUpdateListener {
+                    val valueAnimateRotation = it.getAnimatedValue("rotationX") as Float
+                    val valueColor = it.getAnimatedValue("color") as Int
+                    findViewById<ImageView>(R.id.imageView).rotationX = valueAnimateRotation
+                    findViewById<ImageView>(R.id.imageView).setColorFilter(valueColor)
+                }
+
+            }
+
+        val yAnimator = ValueAnimator.ofFloat(0f, -500f).apply {
+            interpolator = LinearInterpolator()
+            duration = 2000
+            addUpdateListener {
+                val valueAnimationY = it.animatedValue as Float
+                findViewById<ImageView>(R.id.imageView).translationY = valueAnimationY
+            }
+
+        }
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playSequentially(yAnimator, xAnimatorAndColor)
+        animatorSet.start()
+
+    }
+}
